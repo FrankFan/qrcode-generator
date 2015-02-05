7ba2f9f10ed1792e2a9d5c7def017dc593433fcf
fanyong <y.fan@Ctrip.com>
2015-02-05 17:52:10 +0800

    :car: finsih basic qrcode feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
-console.log('come in');
-console.log($);
+$(function(){
+
+    chrome.tabs.getSelected(null, function(tab){
+
+        console.dir(tab);
+
+        var title = tab.title;
+        var url = tab.url;
+
+        $('#qrcode').qrcode({
+            text: url,
+            width: '180',
+            height: '180'
+        });
+
+        $('#title').val(title);
+        $('#url').val(url);
+
+    });
+
+});
