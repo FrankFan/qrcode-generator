cd0392909e3dc97c4a63c67a8de1d7e9dc32d1c0
FrankFan <fanyong@gmail.com>
2015-02-07 00:38:41 +0800

    :rocket: add an awesome new feature: long url to tiny url

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,31 @@ $(function(){
         $('#title').val(title);
         $('#url').val(url);
 
+        // 将url转成短链
+        getShortUrl(url);        
+
+        
     });
+
+    var getShortUrl = function(url_long) {
+        var obj = {};
+        var appkey = '648450463';
+        var api = 'https://api.weibo.com/2/short_url/shorten.json?source={appkey}&url_long={url_long}';
+        api = api.replace(/{appkey}/, appkey).replace(/{url_long}/, encodeURIComponent(url_long));
+        $.ajax({
+            type: 'GET',
+            url: api,
+            dataType: 'json',
+            success: function(data) {
+                if(data && data.urls && data.urls.length > 0) {
+                    obj = data.urls[0];
+                    var tinyUrl = obj.url_short;
+                    $('#tinyUrl').val(tinyUrl);
+                }
+            },
+            error: function(data) {
+            }
+        });
+    };
 
 });
